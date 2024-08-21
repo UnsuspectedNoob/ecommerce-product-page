7eedf1fe28c56404b92835e8f635204b84a43957
UnsuspectedNoob <mathersmarshall948@gmail.com>
2024-08-21 17:10:31 +0100

    Change title and website icon Add colors to the tailwind file
    Begin coding the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div>App</div>;
+  return <div className="text-vd-blue"></div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kumbh: "'Kumbh Sans'",
+      },
+      colors: {
+        orange: "hsl(26, 100%, 55%)",
+        "pale-orange": "hsl(25, 100%, 94%)",
+        "vd-blue": "hsl(220, 13%, 13%)",
+        "dg-blue": "hsl(219, 9%, 45%)",
+        "g-blue": "hsl(220, 14%, 75%)",
+        "lg-blue": "hsl(223, 64%, 98%)",
+      },
+    },
   },
   plugins: [],
 };
