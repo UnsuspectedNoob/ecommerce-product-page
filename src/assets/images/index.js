6c0bd054f4f1615716b450600335ee72c5a02f95
UnsuspectedNoob <mathersmarshall948@gmail.com>
2024-08-21 16:45:07 +0100

    Add assets (fonts, images)

---
+++ src/assets/images/index.js
@@ -0,0 +1,41 @@
+import icon from "./favicon-32x32.png";
+import cart from "./icon-cart.svg";
+import close from "./icon-close.svg";
+import deleteIcon from "./icon-delete.svg";
+import menu from "./icon-menu.svg";
+import minus from "./icon-minus.svg";
+import next from "./icon-next.svg";
+import plus from "./icon-plus.svg";
+import previous from "./icon-previous";
+import avatar from "./image-avatar.png";
+import product1Thumbnail from "./image-product-1-thumbnail.jpg";
+import product2Thumbnail from "./image-product-2-thumbnail.jpg";
+import product3Thumbnail from "./image-product-3-thumbnail.jpg";
+import product4Thumbnail from "./image-product-4-thumbnail.jpg";
+import product1 from "./image-product-1.jpg";
+import product2 from "./image-product-2.jpg";
+import product3 from "./image-product-3.jpg";
+import product4 from "./image-product-4.jpg";
+import logo from "./logo.svg";
+
+export {
+  icon,
+  cart,
+  close,
+  deleteIcon,
+  menu,
+  minus,
+  next,
+  plus,
+  previous,
+  avatar,
+  product1Thumbnail,
+  product2Thumbnail,
+  product3Thumbnail,
+  product4Thumbnail,
+  product1,
+  product2,
+  product3,
+  product4,
+  logo,
+};
